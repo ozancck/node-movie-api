d7038fc9569cb70ce04c18e64a71e439b35a4ce7
Ozan <ozancck@icloud.com>
2022-05-22 17:19:34 +0300

    /api/directors/:director_id DELETE method

--- routes/director.js
+++ routes/director.js
@@ -141,4 +141,17 @@ router.put(`/:director_id`, (req,res,next)=> {
 
 })
 
+router.delete("/:director_id", (req,res,next)=> {
+    const promise = Director.findByIdAndRemove(req.params.director_id);
+
+    promise.then((director) => {
+        if (!director)
+            next({ message: 'The director was not found.', code: 99 });
+
+        res.json({ status: 1 });
+    }).catch((err) => {
+        res.json(err);
+    });
+})
+
 module.exports = router;
