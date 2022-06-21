90a9a6983b78afa97bee40a6ce1b2c4089c6c67e
Ozan <ozancck@icloud.com>
2022-06-21 23:59:05 +0300

    jason web token verify

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ const db = require("./helper/db")()
 const config = require("./config")
 app.set("api_secret_key", config.api_secret_key)
 
+//MiddleWare
+
+const verifyToken = require("./middleware/verify-token")
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -30,6 +34,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use("/api", verifyToken)
 app.use('/api/movies', movie);
 app.use('/api/directors', director);
 
