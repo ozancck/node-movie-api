15925adc86e4466d7c0fc1577cff161787eb0e9c
Ozan <ozancck@icloud.com>
2022-06-25 20:45:43 +0300

    mocha movie test

---
+++ test/api/movie.test.js
@@ -0,0 +1,126 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+const server = require('../../app');
+chai.use(chaiHttp);
+
+let token, movieId;
+
+describe('/api/movies tests', () => {
+    before((done) => {
+        chai.request(server)
+            .post('/authenticate')
+            .send({username: 'ozan2', password: '12345'})
+            .end((err, res) => {
+                token = res.body.token;
+                done()
+            });
+    });
+
+    describe('/GET movies', () => {
+        it('it should GET all the movies', (done) => {
+            chai.request(server)
+                .get('/api/movies')
+                .set('x-access-token', token)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+                    done();
+                });
+        })
+    });
+
+    describe('/POST movie', () => {
+        it('it should POST a movie', (done) => {
+            const movie = {
+                title: 'ozan',
+                director_id: '5a34e1afb8523a786e31f8540',
+                category: 'korku',
+                country: 'Türkiye',
+                year: 1960,
+                imdb_score: 4
+            };
+
+            chai.request(server)
+                .post('/api/movies')
+                .send(movie)
+                .set('x-access-token', token)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('title');
+                    res.body.should.have.property('director_id');
+                    res.body.should.have.property('category');
+                    res.body.should.have.property('country');
+                    res.body.should.have.property('year');
+                    res.body.should.have.property('imdb_score');
+                    movieId = res.body._id;
+                    done();
+                });
+        });
+    });
+
+    describe('/GET/:movie_id movie', () => {
+        it('it should GET a movie by the given id', (done) => {
+            chai.request(server)
+                .get('/api/movies/' + movieId)
+                .set('x-access-token', token)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('title');
+                    res.body.should.have.property('director_id');
+                    res.body.should.have.property('category');
+                    res.body.should.have.property('country');
+                    res.body.should.have.property('year');
+                    res.body.should.have.property('imdb_score');
+                    res.body.should.have.property('_id').eql(movieId);
+                    done();
+                });
+        });
+    });
+
+    describe('/PUT/:movie_id movie', () => {
+        it('it should UPDATE a movie given by id', (done) => {
+            const movie = {
+                title: 'kalemkagıt',
+                director_id: '5a34e1afb8523adsgsd78631f8541',
+                category: 'Action',
+                country: 'Germany',
+                year: 1973,
+                imdb_score: 5
+            };
+
+            chai.request(server)
+                .put('/api/movies/' + movieId)
+                .send(movie)
+                .set('x-access-token', token)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('title').eql(movie.title);
+                    res.body.should.have.property('director_id').eql(movie.director_id);
+                    res.body.should.have.property('category').eql(movie.category);
+                    res.body.should.have.property('country').eql(movie.country);
+                    res.body.should.have.property('year').eql(movie.year);
+                    res.body.should.have.property('imdb_score').eql(movie.imdb_score);
+
+                    done();
+                });
+        });
+    });
+
+    describe('/DELETE/:movie_id movie', () => {
+        it('it should DELETE a movie given by id', (done) => {
+            chai.request(server)
+                .delete('/api/movies/' + movieId)
+                .set('x-access-token', token)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('status').eql(1);
+                    done();
+                });
+        });
+    });
+});
